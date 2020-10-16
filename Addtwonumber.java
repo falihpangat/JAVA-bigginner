f65bace8bc8bfc5afb1d67469ebc992a331151c2
falih pangat <falihp123@gmail.com>
2020-10-16 15:40:31 +0530

    this program show Add two number by usning scanner

---
+++ Addtwonumber.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Addtwonumber{
+    public static void main(String[] args) {
+        int sum;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter numer A");
+        int A = sc.nextInt();
+        System.out.println("enter the number B");
+        int B = sc.nextInt();
+        sum = A + B;
+        System.out.println("the sum of A and B is :" + sum);
+
+    }
+}
