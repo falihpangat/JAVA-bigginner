83c3937d22317c133c65e19750d811a1c1935d4a
falih pangat <falihp123@gmail.com>
2020-10-16 08:55:37 +0530

    this program print single line print (println)

---
+++ Printnumsingleline.java
@@ -0,0 +1,12 @@
+public class Printnumsingleline {
+    
+        public static void main(String args[]) {
+            int N = 5;
+            
+            for (int iter = 0; iter < N; ++iter) {
+                System.out.println(iter + " ");
+            }
+        }
+    
+    
+}
