c6d76e9288d48f501d952d4dc5db7dafd4bfd734
falih pangat <falihp123@gmail.com>
2020-10-16 09:00:53 +0530

    backward print using n--

---
+++ Printnumbacke.java
@@ -0,0 +1,12 @@
+public class Printnumbacke {
+    
+        public static void main(String args[]) {
+            int N = 5;
+            // 'n--' increment help to increament backward //
+            for ( int iter = N; iter > 0; iter-- ) {
+                //we can use only 'print' to  show it in a single line//
+                System.out.println(iter + " ");
+            }
+        }
+    
+}
