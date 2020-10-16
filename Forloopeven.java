14ee0c085d08fd543df276a5672d50ba5d30e43d
falih pangat <falihp123@gmail.com>
2020-10-16 08:42:16 +0530

    even number basic using forloop

---
+++ Forloopeven.java
@@ -0,0 +1,9 @@
+public class Forloopeven {
+    public static void main(String[] args) {
+        for (int n = 2; n <= 100 ; n+=2)
+        {
+            System.out.println("even numbers are " + n);
+}
+}
+    
+}
